76318da7f98065e5db1ac3c156e2c2b14f1e6715
xvvx <q867007988@gmail.com>
2019-04-16 09:11:00 +0800

    增加过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
 <div>
-  <router-view/>
+  <transition mode="out-in">
+    <router-view/>
+  </transition>
 </div>
 </template>
 
 <style lang="scss">
+  .v-enter {
+    opacity: 0;
+  }
 
+  .v-leave-to {
+    opacity: 0;
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: all .4s ease;
+  }
 </style>
