de0f32b11280ada3e3e69de96e92294c5e2e6a7b
xvvx <q867007988@gmail.com>
2019-04-27 18:15:29 +0800

    重构地址选择页面, 优化代码结构, 添加大量注释

--- src/views/City.vue
+++ src/views/City.vue
@@ -14,23 +14,24 @@
     <main class="main">
       <div class="search">
         <form>
-          <input type="search" class="input_search" ref="inputWords" required placeholder="输入学校、商务楼、地址">
+          <input type="search" class="input_search" v-model="keyword" required placeholder="输入学校、商务楼、地址">
           <input type="submit" class="search_btn" @click="showSearch">
         </form>
       </div>
       <div class="search_res">
-        <p class="word" v-if="search_flag">搜索历史</p>
-        <div class="no_res" v-if="res.length === 0">很抱歉! 无搜索结果</div>
+        <!-- 搜索历史 -->
+        <p class="word" v-if="search_flag === 0">搜索历史</p>
+        <!-- 无搜索结果 -->
+        <div class="no_res" v-if="search_flag === 2">很抱歉! 无搜索结果</div>
+        <!-- 搜索记录和历史记录共享组件, 由值 res 渲染 -->
         <div class="show_search">
           <div class="site_box" v-for="item of res" :key="item.geohash" @click="goandsave(item)">
             <p class="site_name">{{ item.name }}</p>
             <p class="site_addr">{{ item.address }}</p>
           </div>
         </div>
-        <!-- 历史记录存在 且在搜索之前 才显示按钮 -->
-        <div class="clear_all" @click="clearAll" v-if="search_his.length !== 0 && search_flag">
-          清空所有
-        </div>
+        <!-- 历史记录存在 才显示按钮 -->
+        <div class="clear_all" @click="clearAll" v-if="search_flag === 0 && search_his.length !== 0">清空所有</div>
       </div>
     </main>  
   </div>
@@ -42,19 +43,22 @@ export default {
   props: ['id'],
   data () {
     return {
-      search_flag: true,
+      // 搜索结果标识, 0 代表历史记录, 1 代表存在搜索结果, 2 代表不存在搜索结果
+      search_flag: 0,
+      // 关键字
+      keyword: '',
+      // 城市信息(顶部标题)
       city_info: {},
+      // 搜索历史, 显示需传值给res
       search_his: [],
-      res: {} // 对象没长度, 这样能使初始状态不显示 no_res
+      // 显示数据
+      res: [] // 对象没长度, 这样能使初始状态不显示 no_res
     }
   },
   created () {
     this.getCityInfo()
-    this.search_his = JSON.parse(window.localStorage.getItem('addr_his'))
-    if (!this.search_his) {
-      this.search_his = [] 
-      return
-    } 
+    var temp_his = window.localStorage.getItem('addr_his')
+    this.search_his = temp_his ? JSON.parse(temp_his) : []
     this.res = this.search_his
   },
   methods: {
@@ -62,57 +66,63 @@ export default {
     back () {
       this.$router.push({path: '/home'})
     },
-    // 显示搜索结果
-    showSearch (e) {
-      if (this.$refs.inputWords.value) {
-        this.search_flag = false
-        // 阻止提交表单默认行为
-        e.preventDefault()
-        this.getSearchInfo()
-      }
-    },
-    // 获取城市信息
+    // 获取城市信息(标题)
     getCityInfo () {
       this.axios.get(`v1/cities/${this.id}`).then(res => {
         this.city_info = res.data
       })
+    },
+    // 显示搜索结果
+    showSearch (e) {
+      // 判断关键字是否为空
+      if (this.keyword === '') return
+      // 阻止提交表单默认行为
+      e.preventDefault()
+      // 获取搜索结果
+      this.getSearchInfo()
     },
     // 获取搜索信息
     getSearchInfo () {
       this.axios.get('v1/pois', {
         params: {
           city_id: this.id,
-          keyword: this.$refs.inputWords.value
+          keyword: this.keyword
         }
       }).then(res => {
         this.res = res.data
+        // 判断是否有返回结果
+        this.isExist()
       })
+    },
+    // 判断是否有返回结果
+    isExist () {
+      if (this.res.length === 0) {
+        this.search_flag = 2
+        return
+      }
+      this.search_flag = 1
     },
     // 本地储存历史地址
     goandsave (item) {
       // 通过标识符来确定该信息是否已被储存
-      var flag = true
-      this.search_his.some(res => {
+      var flag = this.search_his.some(res => {
         // 判断是否有重复
-        if (res.geohash === item.geohash) {
-          flag = false
-          return true
-        }
+        return res.geohash === item.geohash
       })
-      if (flag) {
+      if (!flag) {
         this.search_his.unshift(item)
         window.localStorage.setItem('addr_his', JSON.stringify(this.search_his))
       }
+      // 存储经纬信息
       this.$store.commit('getSiteInfo', item)
+      // 跳转
       this.$router.push({path: '/msite', query: { geohash: item.geohash }})
     },
     // 清除搜索记录
     clearAll () {
       window.localStorage.removeItem('addr_his')
       // 初始化变量
-      // 历史记录为空数组则"清空所有"按钮隐藏
-      this.search_his = []
-      this.res = {}
+      this.search_his = this.res = []
     },
     // 返回上一层
     back () {
@@ -121,6 +131,13 @@ export default {
     // 返回选择城市列表
     changeCity () {
       this.$router.push({path: '/home'})
+    }
+  },
+  watch: {
+    keyword (val) {
+      if (val !== '') return
+      this.search_flag = 0
+      this.res = this.search_his
     }
   },
   components: {
