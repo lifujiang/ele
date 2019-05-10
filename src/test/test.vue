f853758b02423ad302e2a5fb9427c9fe091af0d7
xvvx <q867007988@gmail.com>
2019-05-10 12:18:49 +0800

    完成商店页面左右联动DEMO

--- src/test/test.vue
+++ src/test/test.vue
@@ -1,38 +1,69 @@
 <template>
   <div class="testPage">
-    <cube-scroll-nav
-  :side="true"
-  :data="data"
-  :current="current"
-  @change="changeHandler"
-  @sticky-change="stickyChangeHandler">
-
-  <cube-scroll-nav-panel
-    v-for="item in data"
-    :key="item.id"
-    :label="item.id"
-    :title="item.name">
-    <ul>
-      <li v-for="food in item.foods" :key="food.id">
-        <div>
-          <img :src=" imgsrc + food.image_path">
-          <p>{{food.name}}</p>
-        </div>
-      </li>
-    </ul>
-  </cube-scroll-nav-panel>
-</cube-scroll-nav>
+    <div class="goods">
+      <!-- 侧边导航栏 -->
+      <div class="menu-wrapper">
+        <ul class="item-ul">
+          <!-- current -->
+          <li
+          @click="clickList(index)"
+          ref="menuList"
+          class="menu-item"
+          v-for="(goods, index) in data"
+          :key="goods.id"
+          :class="{current: index === currentIndex}">
+            <span class="menu-title">{{ goods.name }}</span>
+          </li>
+        </ul>
+      </div>
+      <!-- 商品栏 -->
+      <div class="goods-wrapper">
+        <!-- 导航栏标题 -->
+        <ul class="goods-ul">
+          <li>
+            <div>
+              <h4></h4>
+              <h5></h5>
+              <span></span>
+            </div>
+            <ul ref="goodsList">
+              <li class="goods-li" v-for="goods in data" :key="goods.id">
+                <div class="foodCard" v-for="food in goods.foods" :key="food.id">
+                  <div class="left">
+                    <img :src="imgsrc + food.image_path" alt="">
+                  </div>
+                  <div class="right">
+                    <p class="title">{{ food.name }}</p>
+                    <p class="subTitle">{{ food.description }}</p>
+                    <p>月售{{ food.month_sales }}份 好评率{{ food.satisfy_rate }}%</p>
+                    <p class="tag" v-if="food.activity">{{ food.activity.image_text }}</p>
+                    <div class="priceSection">
+                      <p class="left">
+                        <span class="price">&yen;20</span>
+                        <span> 起</span>
+                      </p>
+                      <span>+</span>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
   data () {
     return {
       imgsrc: 'https://elm.cangdu.org/img/',
-      data: [],
-      current: '',
-      id: 1
+      data: undefined,
+      scrollY: 0,
+      rightLiTops: []
     }
   },
   created () {
@@ -40,21 +71,63 @@ export default {
   },
   methods: {
     getGoods () {
-      this.axios.get('shopping/v2/menu', {
-        params: {
-          restaurant_id: this.id
-        }
-      }).then(res => {
+      this.$api.goods({
+        restaurant_id: 1
+      })
+      .then(res => {
         this.data = res.data
-        this.current = this.data[0].id
         console.log(this.data)
       })
+      .catch(err => {
+        this.$api.error(err)
+      })
     },
-     changeHandler(label) {
-      console.log('changed to:', label)
+    _initBScroll () {
+      this.leftBscroll = new BScroll('.menu-wrapper', {click: true});
+      this.rightBscroll = new BScroll('.goods-wrapper', {
+        probeType: 3
+      })
+      this.rightBscroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(pos.y)
+      })
     },
-    stickyChangeHandler(current) {
-      console.log('sticky-change', current)
+    _initRightHeight () {
+      let itemArray = []
+      let top = 0
+      itemArray.push(top)
+      // 获取右边所有的li
+      let allList = this.$refs.goodsList.querySelectorAll('.goods-li')
+      allList.forEach(li => {
+        top += li.clientHeight
+        itemArray.push(top)
+      })
+      this.rightLiTops = itemArray;
+    },
+    _initLeftScroll(index){
+      let menu = this.$refs.menuList;
+      let el = menu[index];
+      this.leftBscroll.scrollToElement(el,300,0,-300)
+    },
+    clickList (index) {
+      this.scrollY = this.rightLiTops[index]
+      this.rightBscroll.scrollTo(0, -this.scrollY, 200)
+    }
+  },
+  watch: {
+    data () {
+      this.$nextTick(() => {
+        this._initBScroll()
+        this._initRightHeight()
+      })
+    }
+  },
+  computed: {
+    currentIndex(index) {
+      const { scrollY, rightLiTops } = this
+      return rightLiTops.findIndex((tops, index) => {
+        this._initLeftScroll(index)
+        return scrollY >= tops && scrollY < rightLiTops[index +1]
+      })
     }
   }
 }
@@ -62,21 +135,53 @@ export default {
 
 <style lang="scss">
   .testPage {
-    .cube-scroll-wrapper {
-      height: 667px;
-    }
     @import '../static/styles/mixin.scss';
-    color: #555;
+    
+    .goods {
+      display: flex;
+      .menu-wrapper {
+        height: 375px;
+        .item-ul{
+          .current {
+            @include bgc(white);
+            &::before {
+              content: '';
+              position: absolute;
+              @include bgc($maincl);
+              width: 5px;
+              height: 60px;
+              left: 0;
+            }
+          }
+          .menu-item {
+            width: 80px;
+            height: 60px;
+            border: 1px solid #ccc;
+            overflow-x: hidden;
+            white-space: nowrap;
+          }
+        }
+      }
+      .goods-wrapper {
+        height: 375px;
+        .goods-ul {
+
+        }
+      }
+    }
     .foodCard {
+      @include bgc(white);
+      border-bottom: 1px solid $bordercl;
       padding: 13px;
       display: flex;
+      font-size: 12px;
+      width: 375px;
       .left {
         margin-right: 10px;
         img {
           display: block;
           width: 50px;
           height: 50px;
-          border: 1px solid green;
         }
       }
       .right {
@@ -93,11 +198,11 @@ export default {
           color: #999;
         }
         .tag {
-          border-radius: 8px;          
+          border-radius: 8px;
           display: inline-block;
           padding: 1px;
-          border: 1px solid red;
-          color: red;
+          border: 1px solid #f07373;
+          color: #f1884f;
         }
         .priceSection {
           @include flexbw;
@@ -105,7 +210,7 @@ export default {
             .price {
               font-size: 16px;
               font-weight: bold;
-              color: rgb(255, 101, 0);
+              color: rgb(255, 121, 24);
             }
           }
         }
